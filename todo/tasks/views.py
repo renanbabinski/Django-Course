ae6f620b0fbaec563960a2e5749de745a0ec7ee6
renanbabinski <renan10babinski@hotmail.com>
2020-10-19 06:17:06 -0300

    Dashboard

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator
 from django.http import HttpResponse
 from .forms import TaskForm
 from django.contrib import messages
+import datetime
 
 from .models import Task
 
@@ -13,6 +14,10 @@ def taskList(request):
 
     search = request.GET.get('search')
     filter = request.GET.get('filter')
+    tasksDoneRecently = Task.objects.filter(done='done', updated_at__gt=datetime.datetime.now()-datetime.timedelta(days=30), user=request.user).count()
+    tasksDone = Task.objects.filter(done='done', user=request.user).count()
+    tasksDoing = Task.objects.filter(done='doing', user=request.user).count()
+
 
     if search:
 
@@ -32,7 +37,7 @@ def taskList(request):
 
         tasks = paginator.get_page(page)
 
-    return render(request, 'tasks/list.html', {'tasks': tasks})
+    return render(request, 'tasks/list.html', {'tasks': tasks, 'tasksrecently': tasksDoneRecently, 'tasksdone': tasksDone, 'tasksdoing': tasksDoing })
 
 @login_required
 def taskView(request, id):
